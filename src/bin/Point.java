dcf168dc95aa9f5a0895a55fc0e8bc7de1af3bb4
hangarita <hangarita@INV025.edistribution.loc>
2015-06-09 15:06:37 -0500

    Creating Rings

--- src/bin/Point.java
+++ src/bin/Point.java
@@ -12,10 +12,56 @@ package bin;
 public class Point {
     int intPoint;
     Ring objRing;
+    
+    int x;
+    int y;
 
     public Point(int intPoint) {
         this.intPoint = intPoint;
         objRing = new Ring(intPoint, intPoint);
     }
+
+    public Point(int intPoint, Ring objRing, int x, int y) {
+        this.intPoint = intPoint;
+        this.objRing = objRing;
+        this.x = x;
+        this.y = y;
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="comment">
+    public int getIntPoint() {
+        return intPoint;
+    }
+    
+    public void setIntPoint(int intPoint) {
+        this.intPoint = intPoint;
+    }
+    
+    public Ring getObjRing() {
+        return objRing;
+    }
+    
+    public void setObjRing(Ring objRing) {
+        this.objRing = objRing;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    public void setY(int y) {
+        this.y = y;
+    }
+//</editor-fold>
+    
+    
     
 }
--- src/bin/Ring.java
+++ src/bin/Ring.java
@@ -5,13 +5,19 @@
  */
 package bin;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author hangarita
  */
 public class Ring {
+
     int intRing;
     int[] intPointsRing;
+
+    HashMap<Integer, Point> lstPoints = new HashMap<>();
 
     public Ring(int intRing, int[] intPointsRing) {
         this.intRing = intRing;
@@ -22,33 +28,87 @@ public class Ring {
         this.intRing = intRing;
         SearchRing(intPoint);
     }
-    
-    public static int[] PointsOfRing(int intPosibleRing){
+
+    private void DefineRing() {
+        //intRingEx[1] = 6 + ( (intPosibleRing -1) * 6 );
+        int pointPerSide = ((intRing - 1));
+        if (pointPerSide < 0) {
+            lstPoints.put(0, new Point(intRing, this, 0, 0));
+        } else {
+            double intStart = 1;
+            if (pointPerSide % 2 != 0) {
+                intStart = 0.5;
+            }
+
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            arrayList.add(intPointsRing[1]);
+            for (int i = intPointsRing[0]; i < intPointsRing[1]; i++) {
+                arrayList.add(i);
+            }
+            
+            ArrayList<Integer> lstPointsVertices = new ArrayList<>();
+            
+            for (int i = 0; i < arrayList.size(); i++) {
+                
+                if(i == lstPointsVertices.get(0)){
+                    lstPoints.put(0, new Point(intRing, this, (pointPerSide + 1) / 2, -intRing));
+                }else if(i < lstPointsVertices.get(1) && i > lstPointsVertices.get(0)){
+                    
+                    
+                    
+                }else if(i == lstPointsVertices.get(1)){
+                    
+                }else if(i < lstPointsVertices.get(2) && i > lstPointsVertices.get(1)){
+                    
+                }else if(i == lstPointsVertices.get(2)){
+                    
+                }else if(i < lstPointsVertices.get(3) && i > lstPointsVertices.get(2)){
+                    
+                }else if(i == lstPointsVertices.get(3)){
+                    
+                }else if(i < lstPointsVertices.get(4) && i > lstPointsVertices.get(3)){
+                    
+                }else if(i == lstPointsVertices.get(4)){
+                    
+                }else if(i < lstPointsVertices.get(5) && i > lstPointsVertices.get(4)){
+                    
+                }else if(i == lstPointsVertices.get(5)){
+                    
+                }
+                
+                lstPoints.put(arrayList.get(i),
+                        new Point(intRing, this, 0, 0));
+            }
+
+        }
+    }
+
+    public static int[] PointsOfRing(int intPosibleRing) {
         int[] intRingEx = new int[2];
-        
-        intRingEx[1] = 6 + ( (intPosibleRing -1) * 6 );
-        
-        if(intPosibleRing > 1){
-            intRingEx[0] = 6 + ( (intPosibleRing -2) * 6 ) + 1;
-        }else{
+
+        intRingEx[1] = 6 + ((intPosibleRing - 1) * 6);
+
+        if (intPosibleRing > 1) {
+            intRingEx[0] = 6 + ((intPosibleRing - 2) * 6) + 1;
+        } else {
             intRingEx[0] = 1;
         }
-        
+
         return intRingEx;
     }
-    
-    private void SearchRing(int intPoint){
-        if(intPoint == 0){
+
+    private void SearchRing(int intPoint) {
+        if (intPoint == 0) {
             intPointsRing[0] = 0;
             intPointsRing[1] = 0;
-        }else{
+        } else {
             boolean isRingable = false;
-            int cont=1;
-            
-            while(!isRingable){
+            int cont = 1;
+
+            while (!isRingable) {
                 int[] PointsOfRing = Ring.PointsOfRing(cont);
-                
-                if(intPoint >= PointsOfRing[0] && intPoint <= PointsOfRing[1]){
+
+                if (intPoint >= PointsOfRing[0] && intPoint <= PointsOfRing[1]) {
                     isRingable = true;
                     intPointsRing = PointsOfRing;
                 }
@@ -56,6 +116,5 @@ public class Ring {
             }
         }
     }
-    
-    
+
 }
